cf053d4e9fae7b122ddf02da9734982a45c82099
aliciapaz <aliciapazrojas@gmail.com>
2021-04-23 17:12:57 -0400

    turn book function into a class

--- script.js
+++ script.js
@@ -1,64 +1,63 @@
 const myLibrary = [];
-const table = document.getElementById('table');
+const table = document.getElementById("table");
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 
-Book.prototype = {
-  ...Book.prototype,
   changeRead() {
     this.read = !this.read;
-  },
+  }
 
   addToLibrary() {
     myLibrary.push(this);
     this.id = myLibrary.indexOf(this);
-  },
+  }
 
   delete() {
     myLibrary.splice(this.id, 1);
     const row = document.getElementById(`bookRow${this.id}`);
     row.remove();
     return myLibrary;
-  },
-};
+  }
+}
 
 function updateStatusButton(button, book) {
-  button.innerHTML = book.read ? 'Read' : 'Not read yet';
+  button.innerHTML = book.read ? "Read" : "Not read yet";
 }
 
 function bookStatusButton(book) {
-  const bookStatusButton = document.createElement('button');
+  const bookStatusButton = document.createElement("button");
   const readCell = document.getElementById(`readCell${book.id}`);
-  bookStatusButton.innerHTML = book.read ? 'Read' : 'Not read yet';
+  bookStatusButton.innerHTML = book.read ? "Read" : "Not read yet";
   readCell.appendChild(bookStatusButton);
-  bookStatusButton.addEventListener('click', () => {
+  bookStatusButton.addEventListener("click", () => {
     book.changeRead();
     updateStatusButton(bookStatusButton, book);
   });
 }
 
 function addDeleteBtn(book) {
-  const deleteBtn = document.createElement('button');
+  const deleteBtn = document.createElement("button");
   const deleteCell = document.getElementById(`deleteCell${book.id}`);
-  deleteBtn.innerHTML = 'Delete';
+  deleteBtn.innerHTML = "Delete";
   deleteCell.appendChild(deleteBtn);
-  deleteBtn.addEventListener('click', () => {
+  deleteBtn.addEventListener("click", () => {
     book.delete();
   });
 }
 
 function displayBooks() {
-  document.querySelectorAll('.bookRow').forEach((el) => el.remove());
+  document.querySelectorAll(".bookRow").forEach((el) => el.remove());
 
   for (let i = 0; i < myLibrary.length; i += 1) {
     const row = table.insertRow();
     row.id = `bookRow${myLibrary[i].id}`;
-    row.className = 'bookRow';
+    row.className = "bookRow";
     const titleCell = row.insertCell();
     const authorCell = row.insertCell();
     const pagesCell = row.insertCell();
@@ -74,28 +73,28 @@ function displayBooks() {
   }
 }
 
-const addBookButton = document.getElementById('addBookButton');
-const addBookModal = document.getElementById('addBookModal');
-const form = document.getElementById('bookForm');
+const addBookButton = document.getElementById("addBookButton");
+const addBookModal = document.getElementById("addBookModal");
+const form = document.getElementById("bookForm");
 
 addBookButton.onclick = function () {
-  addBookModal.style.display = 'flex';
+  addBookModal.style.display = "flex";
 };
 
 window.onclick = function (event) {
   if (event.target === addBookModal) {
-    addBookModal.style.display = 'none';
+    addBookModal.style.display = "none";
   }
 };
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const title = form.elements.title.value;
   const author = form.elements.author.value;
   const pages = form.elements.pages.value;
-  const read = form.elements.readStatus.value === 'true';
+  const read = form.elements.readStatus.value === "true";
   const myBook = new Book(title, author, pages, read);
   myBook.addToLibrary();
   displayBooks();
-  addBookModal.style.display = 'none';
+  addBookModal.style.display = "none";
 });
